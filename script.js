1116c49a039e4746b2be4a0f1545ab1dcd02c847
Glitch (vitaoairlines) <none>
2021-03-16 17:27:22 +0000

    ✊🖐 Checkpoint ./.glitch-assets:29813998/43 ./css/sytle.css:29813998/1435 ./index.html:29813998/2250 ./script.js:29813998/3119

---
+++ script.js
@@ -0,0 +1,131 @@
+const pagina = `<section class="boxes">
+<figure>
+    <img src="imagens/aviao(3).jpg"alt="Avião-Beluga">
+    <figcaption>
+        <h2>Avião-Beluga</h2>
+        <h4>R$337.400</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(5).jpg" alt="Avião-Tubarão">
+    <figcaption>
+        <h2>Avião-Tubarão</h2>
+        <h4>R$163.390</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(11).jpg" alt="Avião-Minimalista">
+    <figcaption>
+        <h2>Avião-Minimalista</h2>
+        <h4>R$71.100</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(14).jpg" alt="Avião 4 andares">
+    <figcaption>
+        <h2>Avião 4 andares</h2>
+        <h4>R$327.500</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(1).jpg" alt="Avião-Delta">
+    <figcaption>
+        <h2>Avião-Delta</h2>
+        <h4>R$450.470</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(40).jpg" alt="Avião-Obeso">
+    <figcaption>
+        <h2>Avião-Obeso</h2>
+        <h4>R$340.000</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(19).jpg" alt="Avião-Resort">
+    <figcaption>
+        <h2>Avião-Resort</h2>
+        <h4>R$879.700</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(20).jpg" alt="Avião-Turbina">
+    <figcaption>
+        <h2>Avião-Turbina</h2>
+        <h4>R$400.000</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(15).jpg" alt=""O Avião>
+    <figcaption>
+        <h2>O Avião</h2>
+        <h4>R$1.324.000</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(13).jpg" alt="Avião-Bicicleta">
+    <figcaption>
+        <h2>Avião-Bicicleta</h2>
+        <h4>R$10.000</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(17).jpg" alt="Avião 6 andares">
+    <figcaption>
+        <h2>Avião 6 andares</h2>
+        <h4>R$618.800</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(8).jpg" alt="Avião-Ventilador">
+    <figcaption>
+        <h2>Avião-Ventilador</h2>
+        <h4>R$120.200</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(6).jpg" alt="Avião-Dirigível">
+    <figcaption>
+        <h2>Avião-Dirigível</h2>
+        <h4>R$390.500</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(4).jpg" alt="Avião-Porco">
+    <figcaption>
+        <h2>Avião-Porco</h2>
+        <h4>R$170.000</h4>
+    </figcaption>
+</figure>	
+<figure>
+    <img src="imagens/aviao(24).jpg" alt="Avião-Águia">
+    <figcaption>
+        <h2>Avião-Águia</h2>
+        <h4>R$190.150</h4>
+    </figcaption>
+</figure>	
+<figure>
+    <img src="imagens/aviao(7).jpg" alt="Avião-Pássaro">
+    <figcaption>
+        <h2>Avião-Pássaro</h2>
+        <h4>R$230.000</h4>
+    </figcaption>
+</figure>	
+<figure>
+    <img src="imagens/aviao(10).jpg" alt="Mini-Avião">
+    <figcaption>
+        <h2>Mini-Avião</h2>
+        <h4>R$50.000</h4>
+    </figcaption>
+</figure>
+<figure>
+    <img src="imagens/aviao(22).jpg" alt="Avião-Nave">
+    <figcaption>
+        <h2>Avião-Nave</h2>
+        <h4>R$1.060.900</h4>
+    </figcaption>
+</figure>
+</section>`
+
+document.write(pagina)
+
